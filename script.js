3ba4a5a1f0ba966741ad3338a19a4a1b5d60540d
nafik06 <muhamadnafik12.1ipa@gmail.com>
2025-11-09 01:09:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const iconmenu = document.getElementById("iconmenu");
+const menulist = document.getElementById("menulist");
+
+iconmenu.addEventListener("click", () => {
+    menulist.classList.toggle("hiden");
+});
+
+
