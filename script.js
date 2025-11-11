22c998d67b27491ed1f9dbe6f04c0586f386b81b
nafik06 <muhamadnafik12.1ipa@gmail.com>
2025-11-12 00:53:19 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const iconmenu = document.getElementById("iconmenu");
-const menulist = document.getElementById("menulist");
-
-iconmenu.addEventListener("click", () => {
-    menulist.classList.toggle("hiden");
-});
-
-
